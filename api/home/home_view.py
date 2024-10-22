a2959b44bbbded0568c511fce8135f522e483974
ILuna22 <isaaclunahdz14@gmail.com>
2024-10-22 16:24:50 -0600

    Plantilla Agregada

--- api/home/home_view.py
+++ api/home/home_view.py
@@ -9,11 +9,11 @@ def home_views(request):
     # Define una vista llamada `login_view` que toma el argumento `request`. 
     # `request` es un objeto que contiene todos los datos asociados con la solicitud HTTP.
 
-    template_view = "index.html"
+    template_name = "index.html"
     # Asigna el nombre de la plantilla que queremos renderizar, en este caso "login.html". 
     # `template_name` es una variable que hace referencia a la ubicación del archivo HTML.
 
-    return render(request, template_name=template_view)
+    return render(request, template_name)
     # La función `render` genera una respuesta HTTP que contiene el contenido de la plantilla HTML.
     # Toma dos argumentos principales:
     # 1. `request`: El objeto de solicitud.
